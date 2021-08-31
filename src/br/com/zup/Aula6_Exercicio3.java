13f8659a5713c8bfe3f6500d66260e4b1a201f26
Bárbara Ann Lima <babiplima@gmail.com>
2021-08-31 16:17:45 -0300

    Quarto commit dia aula 6 exercícios

--- src/br/com/zup/Aula6_Exercicio3.java
+++ src/br/com/zup/Aula6_Exercicio3.java
@@ -2,11 +2,11 @@ package br.com.zup;
 
 import java.util.Scanner;
 
-public class Aula6_Exercicio2 {
+public class Aula6_Exercicio3 {
 
     public static void main(String[] args) {
 
-        //Enunciando: Faça um programa que leia números inteiros e imprima sua média.
+        //Faça um programa que leia n números inteiros positivos e calcule a soma desses números.
 
         //Estanciando o método scanner
 
@@ -16,27 +16,32 @@ public class Aula6_Exercicio2 {
 
         int qtnNumeros = 0;
         double soma = 0;
-        double media = 0;
         int contador = 1;
+        double numeroNegativo = 0;
 
-        //Pedindo ao usuário que diga quantos números ele deseja a tirar a média
 
         System.out.println("Por favor, digite a quantidade de números: ");
         qtnNumeros = leitor.nextInt();
 
-        //Criando a estrutura de repetição para somar a quantidade de números inputadas pelo usuário e
-        // após ter a média
+        //Criando a estrutura de repetição para somar a quantidade de números inputadas pelo usuário
+
 
         while (contador <= qtnNumeros) {
             System.out.println("Digite o número " + contador + " : ");
             int valorDadoPeloUsuario = leitor.nextInt();
+            if (valorDadoPeloUsuario > 0) {
+
+            } else {
+                System.out.println("Valor inválido");
+
+            }
+
             soma = soma + valorDadoPeloUsuario;
             contador++;
-            media = soma / valorDadoPeloUsuario;
         }
 
-        //Imprimindo a média
+        //Imprimindo a soma
 
-        System.out.println("A média de todos os números é: " + Math.round(media));
+        System.out.println("A soma de todos os números é: " + Math.round(soma));
     }
 }
