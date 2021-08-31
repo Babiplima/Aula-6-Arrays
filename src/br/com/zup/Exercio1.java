93bbc3ce05e664f2acf1afb540401262b0003480
Bárbara Ann Lima <babiplima@gmail.com>
2021-08-31 14:41:04 -0300

    Segundo commit aula 06 arrays

--- src/br/com/zup/Exercio1.java
+++ src/br/com/zup/Exercio1.java
@@ -4,23 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Exercio1 {
+    //Criando as variáveis
+
     public static void main(String[] args) {
+
         List<Integer> todosOsNumeros = new ArrayList<>();
         List<Integer> numerosPares = new ArrayList<>();
         List<Integer> numerosImpares = new ArrayList<>();
+
+        //Adicionando os números na lista
 
         todosOsNumeros.add(70);
         todosOsNumeros.add(5);
         todosOsNumeros.add(8);
         todosOsNumeros.add(10);
 
-        for (Integer referencia:todosOsNumeros){
-            if (referencia % 2 ==0 ){
+        //Criando a condição para o programa apresentar o que for determinado
+
+        for (Integer referencia : todosOsNumeros) {
+            if (referencia % 2 == 0) {
                 numerosPares.add(referencia);
-            }else{
+            } else {
                 numerosImpares.add(referencia);
             }
         }
+
+        // Imprimindo os números e números pares e números impares
+
         System.out.println("Lista de números: ");
         System.out.println(todosOsNumeros);
         System.out.println("Números impares: ");
